ec46cc0bee6e0a6ca77ec31970edacb6214b208e
Krysztal Huang <krysztal.huang@outlook.com>
2026-01-25 23:19:32 +0800

    fix(layout): fix layout of `AboutPage`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,40 @@
-import mdx from "@astrojs/mdx";
-
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import icon from "astro-icon";
+
+// expressive-code
 import expressiveCode from "astro-expressive-code";
 import { pluginCollapsibleSections } from "@expressive-code/plugin-collapsible-sections";
 import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
 
-import icon from "astro-icon";
+// remark
+import { remarkReadingTime } from "./src/plugin/reading-time.mjs";
+import remarkToc from "remark-toc";
+
+// rehype
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeSlug from "rehype-slug";
 
 export default defineConfig({
     integrations: [
         expressiveCode({
             plugins: [pluginCollapsibleSections(), pluginLineNumbers()],
         }),
+
         mdx(),
         icon(),
     ],
+    markdown: {
+        remarkPlugins: [
+            remarkReadingTime,
+            [remarkToc, { heading: "contents" }],
+        ],
+        remarkPlugins: [
+            rehypeSlug,
+            [rehypeAutolinkHeadings, { behavior: "append" }],
+        ],
+    },
     vite: { plugins: [tailwindcss()] },
     experimental: {
         svgo: true,
