71d6cb7235aa6448c34f427804b4e811dc27ca70
Krysztal Huang <krysztal.huang@outlook.com>
2026-01-28 16:31:04 +0800

    fix(plugin): fix failure of `reading-time.js`

--- src/plugin/reading-time.mjs
+++ src/plugin/reading-time.mjs
@@ -1,10 +1,10 @@
-import MDAST from "mdast-util-to-string";
+import { toString as mdastToString } from "mdast-util-to-string";
 
 import getReadingTime from "reading-time";
 
 export function remarkReadingTime() {
     return (tree, { data }) => {
-        const textOnPage = MDAST.toString(tree);
+        const textOnPage = mdastToString(tree);
         const readingTime = getReadingTime(textOnPage);
 
         data.astro.frontmatter.minutesRead = readingTime.text;
