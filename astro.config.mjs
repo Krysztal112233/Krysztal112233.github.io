cea066ada81899dfc1ff57027b5824317082a4e2
Krysztal Huang <krysztal.huang@outlook.com>
2026-06-14 13:14:01 +0800

    feat(astro): bump to Astro 6.4.6

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,30 @@
-import { defineConfig } from "astro/config";
-import icon from "astro-icon";
+import sitemap from "@astrojs/sitemap";
+import { pluginCollapsibleSections } from "@expressive-code/plugin-collapsible-sections";
+import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
 import tailwindcss from "@tailwindcss/vite";
-
+import { defineConfig } from "astro/config";
 // expressive-code
 import expressiveCode from "astro-expressive-code";
+import icon from "astro-icon";
 import { pluginLanguageBadge } from "expressive-code-language-badge";
-import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
-import { pluginCollapsibleSections } from "@expressive-code/plugin-collapsible-sections";
-
 // rehype
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeMathjax from "rehype-mathjax";
-
 // remark
 import remarkBlock from "remark-github-beta-blockquote-admonitions";
 import remarkMath from "remark-math";
 import remarkToc from "remark-toc";
-import { remarkReadingTime } from "./src/plugin/reading-time.mjs";
 import { getConfig } from "./src/config";
-
-import sitemap from "@astrojs/sitemap";
+import { remarkReadingTime } from "./src/plugin/reading-time.mjs";
 
 const {
     site: { site },
 } = await getConfig();
 
 export default defineConfig({
+    devToolbar: {
+        enabled: false,
+    },
     site: site,
     integrations: [
         expressiveCode({
@@ -53,4 +52,3 @@ export default defineConfig({
     },
     vite: { plugins: [tailwindcss()] },
 });
-
